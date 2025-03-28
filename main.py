f7f5aa74107c340f28e3444880e0d3e019636bfc
Stephan_Zeibig <stephanzeibig@gmx.de>
2025-03-28 20:51:26 +0100

    B: add player class

--- main.py
+++ main.py
@@ -2,21 +2,30 @@ import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
 from constants import *
+from player import *
 
 
 def main():
     
     pygame.init()
     
+    time_object = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     
     while True:
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+        
         screen.fill((0, 0, 0))
+        player.draw(screen)
+        
+        delta_time = time_object.tick(60)
+        dt = delta_time / 1000
+        
         pygame.display.flip()
    
         
